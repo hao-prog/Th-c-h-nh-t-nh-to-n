c5e5208c4c8887c0444cc928371ca317affdcc1f
hao-prog <72394775+hao-prog@users.noreply.github.com>
2021-11-24 17:25:54 +0700

    Delete GiuaKy directory

--- GiuaKy/accuracy.m
+++
@@ -1,6 +0,0 @@
-function [a] = accuracy(y_true, y_pred)
-  
-  match = (y_true == y_pred);
-  a = mean(match)*100;
-  
-end
--- GiuaKy/predict.m
+++
@@ -1,8 +0,0 @@
-function [p] = predict(X, w)
-  coa = 0.75;
-  bias2 = ones(1, size(X)(2));
-  X = [bias2; X];
-  z = w' * X;
-  p = sigmoid(z);
-  p = p > coa;
-end
